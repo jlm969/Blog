795a9d82362d781ddd7ac78f85a9daa8eda44d29
Jorge Marquez <jlm969969@gmail.com>
2022-04-13 21:39:55 -0300

    Se cargaron datos a la BD a tarves del Panel de Admin de Django

--- appPost/admin.py
+++ appPost/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from appPost.models import Post, Comentario, VistaPost, MeGusta
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comentario)
+admin.site.register(VistaPost)
+admin.site.register(MeGusta)
--- appPost/models.py
+++ appPost/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+#from django.contrib.auth.models import User 
+#from django.contrib.auth.forms import UserCreationForm
+#from django import forms
 
 # Create your models here.
+
+# Creo mi propio formulario para registro de usuarios a partir de la clase formulario 
+# UserCreationForm
+
+#class  Usuario(User):
+#   pass
+
+#   def __str__(self):
+#         return self.user_name
+    # email = forms.EmailField()
+    # password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput) 
+    # password2 = forms.CharField(label="Repetir Contraseña", widget=forms.PasswordInput)  
+
+    # class Meta:
+    #     model = User
+    #     fields = ['username', 'email', 'password1','password2']
+    #     #Sacar los mensajes de ayuda
+    #     help_texts = {k:"" for k in fields}
+
+
+
+class Post(models.Model):
+
+    titulo = models.CharField(max_length=80)
+    contenido = models.TextField()
+    imagen = models.ImageField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    ultima_actualizacion = models.DateTimeField(auto_now=True)
+   # autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    fecha_comentario = models.DateTimeField(auto_now_add=True)
+    contenido = models.TextField()
+
+ #   def __str__(self):
+ #       return self.usuario.username
+
+class VistaPost(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    fecha_vista = models.DateTimeField(auto_now_add=True)
+
+#  def __str__(self):
+#     return self.usuario.username  
+
+class MeGusta(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+  #  def __str__(self):
+  #      return self.usuario.user_name  
